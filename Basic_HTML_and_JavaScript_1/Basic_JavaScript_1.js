b6dbdc59a2292d1ebf70ddfe6eaf36f76fbe4e2e
RJHGO <53460219+RJHGO@users.noreply.github.com>
2019-08-01 20:34:58 -0700

    Add files via upload
    Tech Academy html/JavaScript assignments

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,14 @@
+<script>
+alert("read this");
+</script>
+<script>
+	funtion My_First_Function(){
+	var String = "Kiss me, I'm Irish!";
+	var result = String.Fontcolor("green");
+	document.getElementById("Irish").innerHTML =
+	RESULT;
+	}
+	</script>
+
+
+
